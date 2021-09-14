86d393191cd1b2aeab9e1aafe661456f8fee242e
Alexander Chancayauri. Gincho <ginchoalexch@gmail.com>
2021-09-14 17:19:50 -0500

    Creando los Modelos Usuario y Mascota

---
+++ Practica_Nro2/Models/Mascota.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica_Nro2.Models
+{
+    public class Mascota
+    {
+        public string Tipo { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
---
+++ Practica_Nro2/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica_Nro2.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Telefono { get; set; }
+        public string Distrito { get; set; }
+        public string Direccion { get; set; }
+        public Mascota Mascota { get; set; }
+
+    }
+}
